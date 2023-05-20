0df09407b497e15e278790aff41794a1b889aa12
Song Hyunbin <shbin05@naver.com>
2023-05-21 05:45:53 +0900

    order changed

--- frontend/screens/Home.js
+++ frontend/screens/Home.js
@@ -24,336 +24,337 @@ const PlaceText = styled.Text`
 `;
 
 export default function Home({ navigation, route }) {
-  const [infoVisible, setInfoVisible] = useState(false);
-  const [place, setPlace] = useState(null);
-  const [desc, setDesc] = useState(null);
-  const [location, setLocation] = useState({
-    latitude: 37.293787,
-    longitude: 126.974317,
-    longitudeDelta: 0.002,
-    latitudeDelta: 0.002,
-  });
-  const [locationSubscription, setLocationSubscription] = useState(null);
+	const [map, setMap] = useState(true)
+	const [infoVisible, setInfoVisible] = useState(false);
+	const [place, setPlace] = useState(null);
+	const [desc, setDesc] = useState(null);
+	const [location, setLocation] = useState({
+		latitude: 37.293787,
+		longitude: 126.974317,
+		longitudeDelta: 0.002,
+		latitudeDelta: 0.002,
+	});
 
-  const [distanceStates, setDistanceStates] = useState({
-    routeCoordinates: [],
-    distanceTravelled: 0,
-    prevLatLng: {},
-    coordinate: new AnimatedRegion({
-      latitude: 37.293787,
-      longitude: 126.974317,
-    }),
-  });
+	function SelectInfo(id) {
+		setInfoVisible(!infoVisible);
+		setPlace(info[id].place);
+		setDesc(info[id].desc);
+	}
 
-  const { startActivated } = route.params || {}; // MissionMarthon 페이지에서 넘어온 startActivated params
-  console.log("startActivated from Mission Marathon :", startActivated);
-  //   console.log(
-  //     "Route Length",
-  //     distanceStates.routeCoordinates.length,
-  //     "Last Route :",
-  //     distanceStates.routeCoordinates[distanceStates.routeCoordinates.length - 1]
-  //   );
+	const [locationSubscription, setLocationSubscription] = useState(null);
+	const [distanceStates, setDistanceStates] = useState({
+		routeCoordinates: [],
+		distanceTravelled: 0,
+		prevLatLng: {},
+		coordinate: new AnimatedRegion({
+		latitude: 37.293787,
+		longitude: 126.974317,
+		}),
+	});
 
-  async function getCurrentLocation() {
-    console.log("Getting location...");
-    try {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        console.log("Location permission denied");
-        return;
-      }
+	const { startActivated } = route?.params || {}; // MissionMarthon 페이지에서 넘어온 startActivated params
+	console.log("startActivated from Mission Marathon :", startActivated);
+	//   console.log(
+	//     "Route Length",
+	//     distanceStates.routeCoordinates.length,
+	//     "Last Route :",
+	//     distanceStates.routeCoordinates[distanceStates.routeCoordinates.length - 1]
+	//   );
 
-      console.log("Permission granted...");
-      const subscription = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          timeInterval: 300, // update every 0.3 seconds, 버벅거리면 5 sec으로 변경
-        },
-        (location) => {
-          const { latitude, longitude } = location.coords;
-          const newCoordinate = {
-            latitude,
-            longitude,
-          };
-          setLocation({
-            latitude,
-            longitude,
-            latitudeDelta: 0.002,
-            longitudeDelta: 0.002,
-          });
-          setDistanceStates({
-            routeCoordinates: distanceStates.routeCoordinates.concat([
-              newCoordinate,
-            ]),
-            distanceTravelled:
-              distanceStates.distanceTravelled + calcDistance(newCoordinate),
-            prevLatLng: newCoordinate,
-          });
+	async function getCurrentLocation() {
+		console.log("Getting location...");
+		try {
+		const { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			console.log("Location permission denied");
+			return;
+		}
 
-          console.log(
-            "prevLatLng : ",
-            distanceStates.prevLatLng,
-            " newCoord : ",
-            newCoordinate
-          );
-          // if Mission Marthon has Started => activate markUserRoute()
-        }
-      );
+		console.log("Permission granted...");
+		const subscription = await Location.watchPositionAsync(
+			{
+			accuracy: Location.Accuracy.High,
+			timeInterval: 300, // update every 0.3 seconds, 버벅거리면 5 sec으로 변경
+			},
+			(location) => {
+			const { latitude, longitude } = location.coords;
+			const newCoordinate = {
+				latitude,
+				longitude,
+			};
+			setLocation({
+				latitude,
+				longitude,
+				latitudeDelta: 0.002,
+				longitudeDelta: 0.002,
+			});
+			setDistanceStates({
+				routeCoordinates: distanceStates.routeCoordinates.concat([
+				newCoordinate,
+				]),
+				distanceTravelled:
+				distanceStates.distanceTravelled + calcDistance(newCoordinate),
+				prevLatLng: newCoordinate,
+			});
 
-      setLocationSubscription(subscription);
-    } catch (err) {
-      console.warn(err);
-    }
-  }
+			console.log(
+				"prevLatLng : ",
+				distanceStates.prevLatLng,
+				" newCoord : ",
+				newCoordinate
+			);
+			// if Mission Marthon has Started => activate markUserRoute()
+			}
+		);
 
-  //거리 계산
-  function calcDistance(newLatLng) {
-    const { prevLatLng } = distanceStates;
-    console.log("Haversine Calculating,,, ");
-    return haversine(prevLatLng, newLatLng) || 0;
-  }
+		setLocationSubscription(subscription);
+		} catch (err) {
+		console.warn(err);
+		}
+	}
 
-  useEffect(() => {
-    console.log("Calling getCurrentLocation from useEffect...");
-    getCurrentLocation();
-    return () => {
-      if (locationSubscription) {
-        locationSubscription.remove();
-      }
-    };
-  }, []);
+	//거리 계산
+	function calcDistance(newLatLng) {
+		const { prevLatLng } = distanceStates;
+		console.log("Haversine Calculating,,, ");
+		return haversine(prevLatLng, newLatLng) || 0;
+	}
 
-  function SelectInfo(id) {
-    setInfoVisible(!infoVisible);
-    setPlace(info[id].place);
-    setDesc(info[id].desc);
-  }
+	useEffect(() => {
+		console.log("Calling getCurrentLocation from useEffect...");
+		getCurrentLocation();
+		return () => {
+			if (locationSubscription) {
+				locationSubscription.remove();
+			}
+		};
+	}, []);
 
-  if (!location) {
-    return null;
-  }
+	if (!location) {
+		return <View />;
+	}
 
-  return (
-    <Container>
-      <MapView
-        style={{ width: "100%", height: "100%" }}
-        region={location}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        followsUserLocation
-        loadingEnabled
-      >
-        {/* routeCoordinates array를 통해 Polyline을 그립니다 */}
-        <Polyline
-          coordinates={distanceStates.routeCoordinates}
-          strokeWidth={10}
-        />
+	return (
+		<Container>
+			<MapView
+				style={{ width: "100%", height: "100%" }}
+				region={location}
+				provider={PROVIDER_GOOGLE}
+				showsUserLocation
+				followsUserLocation
+				loadingEnabled
+			>
+				{/* routeCoordinates array를 통해 Polyline을 그립니다 */}
+				<Polyline
+				coordinates={distanceStates.routeCoordinates}
+				strokeWidth={10}
+				/>
 
-        {/* <Marker.Animated
-          ref={(marker) => {
-            this.marker = marker;
-          }}
-          coordinate={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-          }}
-        /> */}
+				{/* <Marker.Animated
+				ref={(marker) => {
+					this.marker = marker;
+				}}
+				coordinate={{
+					latitude: location.latitude,
+					longitude: location.longitude,
+				}}
+				/> */}
 
-        <Marker
-          coordinate={{
-            latitude: 37.293948,
-            longitude: 126.974881,
-          }}
-          pinColor="black"
-          onPress={() => SelectInfo(0)}
-        />
+				<Marker
+				coordinate={{
+					latitude: 37.293948,
+					longitude: 126.974881,
+				}}
+				pinColor="black"
+				onPress={() => SelectInfo(0)}
+				/>
 
-        {/* {location && (
-          <Marker
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}
-            title="Current Location"
-          >
-            <Image
-              source={require("../assets/image/currentLocation.png")}
-              style={{ height: 35, width: 35 }}
-            />
-          </Marker>
-        )} */}
-      </MapView>
-      <View
-        style={{
-          position: "absolute",
-          width: "90%",
-          top: "3%",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Shadow distance={5} offset={[3, 3]}>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "white",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "center",
-              width: 90,
-              height: 45,
-              borderRadius: 15,
-            }}
-          >
-            <Text>맵 모드</Text>
-            <Image
-              source={require("../assets/image/map.png")}
-              style={{
-                width: 20,
-                height: 20,
-                marginLeft: 10,
-              }}
-            />
-          </TouchableOpacity>
-        </Shadow>
-        <TouchableOpacity
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          onPress={() => navigation.navigate("Profile")}
-        >
-          <Image
-            source={require("../assets/image/profile.png")}
-            style={{
-              width: 40,
-              height: 40,
-            }}
-          />
-        </TouchableOpacity>
-      </View>
-      {infoVisible ? (
-        <View
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "20%",
-            bottom: "0%",
-          }}
-        >
-          <Shadow
-            distance={8}
-            offset={[0, -2]}
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <View
-              style={{
-                width: "100%",
-                height: "100%",
-                backgroundColor: "white",
-                borderTopStartRadius: 20,
-                borderTopEndRadius: 20,
-                alignItems: "center",
-              }}
-            >
-              <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  width: "90%",
-                  marginTop: 20,
-                }}
-              >
-                <PlaceText>{place}</PlaceText>
-                <TouchableOpacity
-                  style={{
-                    alignItems: "center",
-                    justifyContent: "center",
-                    backgroundColor: "lightgray",
-                    paddingStart: 15,
-                    paddingEnd: 15,
-                    paddingTop: 5,
-                    paddingBottom: 5,
-                    borderRadius: 10,
-                  }}
-                >
-                  <Image
-                    source={require("../assets/image/vector.png")}
-                    style={{
-                      width: 18,
-                      height: 25,
-                    }}
-                  />
-                </TouchableOpacity>
-              </View>
-              <Text style={{ width: "90%", marginTop: 15 }}>{desc}</Text>
-            </View>
-          </Shadow>
-        </View>
-      ) : (
-        <View
-          style={{
-            position: "absolute",
-            width: "90%",
-            bottom: "3%",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <Shadow distance={5} offset={[3, 3]}>
-            <TouchableOpacity
-              style={{
-                backgroundColor: "white",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 40,
-                borderRadius: 15,
-                paddingStart: 10,
-                paddingEnd: 10,
-              }}
-            >
-              <Text>움직인 거리 1.1km</Text>
-            </TouchableOpacity>
-          </Shadow>
-          <Shadow distance={5} offset={[3, 3]}>
-            <TouchableOpacity
-              style={{
-                backgroundColor: "white",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 40,
-                borderRadius: 15,
-                paddingStart: 10,
-                paddingEnd: 10,
-              }}
-            >
-              <Text>방문한 장소</Text>
-            </TouchableOpacity>
-          </Shadow>
-          <Shadow distance={5} offset={[3, 3]}>
-            <TouchableOpacity
-              style={{
-                backgroundColor: "white",
-                alignItems: "center",
-                justifyContent: "center",
-                height: 40,
-                borderRadius: 15,
-                paddingStart: 10,
-                paddingEnd: 10,
-              }}
-              onPress={() => navigation.navigate("Mission")}
-            >
-              <Text>오늘의 미션</Text>
-            </TouchableOpacity>
-          </Shadow>
-        </View>
-      )}
-    </Container>
-  );
+				{/* {location && (
+				<Marker
+					coordinate={{
+					latitude: location.latitude,
+					longitude: location.longitude,
+					}}
+					title="Current Location"
+				>
+					<Image
+					source={require("../assets/image/currentLocation.png")}
+					style={{ height: 35, width: 35 }}
+					/>
+				</Marker>
+				)} */}
+			</MapView>
+			<View
+				style={{
+				position: "absolute",
+				width: "90%",
+				top: "3%",
+				display: "flex",
+				flexDirection: "row",
+				justifyContent: "space-between",
+				}}
+			>
+				<Shadow distance={5} offset={[3, 3]}>
+				<TouchableOpacity
+					style={{
+					backgroundColor: "white",
+					display: "flex",
+					flexDirection: "row",
+					alignItems: "center",
+					justifyContent: "center",
+					width: 90,
+					height: 45,
+					borderRadius: 15,
+					}}
+				>
+					<Text>맵 모드</Text>
+					<Image
+					source={require("../assets/image/map.png")}
+					style={{
+						width: 20,
+						height: 20,
+						marginLeft: 10,
+					}}
+					/>
+				</TouchableOpacity>
+				</Shadow>
+				<TouchableOpacity
+				style={{
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+				onPress={() => navigation.navigate("Profile")}
+				>
+				<Image
+					source={require("../assets/image/profile.png")}
+					style={{
+					width: 40,
+					height: 40,
+					}}
+				/>
+				</TouchableOpacity>
+			</View>
+			{infoVisible ? (
+				<View
+				style={{
+					position: "absolute",
+					width: "100%",
+					height: "20%",
+					bottom: "0%",
+				}}
+				>
+				<Shadow
+					distance={8}
+					offset={[0, -2]}
+					style={{
+					width: "100%",
+					height: "100%",
+					}}
+				>
+					<View
+					style={{
+						width: "100%",
+						height: "100%",
+						backgroundColor: "white",
+						borderTopStartRadius: 20,
+						borderTopEndRadius: 20,
+						alignItems: "center",
+					}}
+					>
+					<View
+						style={{
+						display: "flex",
+						flexDirection: "row",
+						justifyContent: "space-between",
+						width: "90%",
+						marginTop: 20,
+						}}
+					>
+						<PlaceText>{place}</PlaceText>
+						<TouchableOpacity
+						style={{
+							alignItems: "center",
+							justifyContent: "center",
+							backgroundColor: "lightgray",
+							paddingStart: 15,
+							paddingEnd: 15,
+							paddingTop: 5,
+							paddingBottom: 5,
+							borderRadius: 10,
+						}}
+						>
+						<Image
+							source={require("../assets/image/vector.png")}
+							style={{
+							width: 18,
+							height: 25,
+							}}
+						/>
+						</TouchableOpacity>
+					</View>
+					<Text style={{ width: "90%", marginTop: 15 }}>{desc}</Text>
+					</View>
+				</Shadow>
+				</View>
+			) : (
+				<View
+				style={{
+					position: "absolute",
+					width: "90%",
+					bottom: "3%",
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+				}}
+				>
+				<Shadow distance={5} offset={[3, 3]}>
+					<TouchableOpacity
+					style={{
+						backgroundColor: "white",
+						alignItems: "center",
+						justifyContent: "center",
+						height: 40,
+						borderRadius: 15,
+						paddingStart: 10,
+						paddingEnd: 10,
+					}}
+					>
+					<Text>움직인 거리 1.1km</Text>
+					</TouchableOpacity>
+				</Shadow>
+				<Shadow distance={5} offset={[3, 3]}>
+					<TouchableOpacity
+					style={{
+						backgroundColor: "white",
+						alignItems: "center",
+						justifyContent: "center",
+						height: 40,
+						borderRadius: 15,
+						paddingStart: 10,
+						paddingEnd: 10,
+					}}
+					>
+					<Text>방문한 장소</Text>
+					</TouchableOpacity>
+				</Shadow>
+				<Shadow distance={5} offset={[3, 3]}>
+					<TouchableOpacity
+					style={{
+						backgroundColor: "white",
+						alignItems: "center",
+						justifyContent: "center",
+						height: 40,
+						borderRadius: 15,
+						paddingStart: 10,
+						paddingEnd: 10,
+					}}
+					onPress={() => navigation.navigate("Mission")}
+					>
+					<Text>오늘의 미션</Text>
+					</TouchableOpacity>
+				</Shadow>
+				</View>
+			)}
+		</Container>
+	);
 }
